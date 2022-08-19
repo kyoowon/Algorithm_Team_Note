afa8b398f9f542702035f3a2f01c8fe3e6a5af63
kyulee <kyoowon95@gmail.com>
2022-08-19 17:22:32 +0900

    add(0819) : Study Backtracking (https://www.acmicpc.net/workbook/by/BaaaaaaaaaaarkingDog)

---
+++ JAVA/완탐/BJ_S1_16987_계란으로계란치기.java
@@ -0,0 +1,18 @@
+package 완탐;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author kyulee
+ * @link https://www.acmicpc.net/problem/16987
+ * @performance
+ * @category
+ * @memo
+ */
+
+public class BJ_S1_16987_계란으로계란칙 {
+	static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
+	}
+}
